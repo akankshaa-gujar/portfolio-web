12f65eebcefd37f2bf04fe55f1728c5500a3ad33
Akankshagujar17 <akankshagujar1717@gmail.com>
2025-06-13 15:20:02 +0530

    Cleaned up repository by moving files to root and removing old subfolder.

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-function toggleMenu() {
-  const menu = document.querySelector('.menu-links');
-  const icon = document.querySelector('.hamburger-icon');
+// function toggleMenu() {
+//   const menu = document.querySelector('.menu-links');
+//   const icon = document.querySelector('.hamburger-icon');
 
-  menu.classList.toggle('open');
-  icon.classList.toggle('open');
-}
+//   menu.classList.toggle('open');
+//   icon.classList.toggle('open');
+// }
 
+
+  function toggleMenu() {
+    const menu = document.querySelector('.menu-links');
+    menu.classList.toggle('active');
+  }
+
+
+
+  const form = document.getElementById('contact-form');
+  const successMessage = document.getElementById('success-message');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    fetch(form.action, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json'
+      },
+      body: new FormData(form)
+    }).then(response => {
+      if (response.ok) {
+        form.reset();
+        successMessage.style.display = 'block';
+      } else {
+        alert("Oops! Something went wrong.");
+      }
+    });
+  });
